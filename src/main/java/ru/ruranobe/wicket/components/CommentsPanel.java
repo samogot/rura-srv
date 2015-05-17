a955562ab216e1e762416ccc9a20d6769dc1ece9
samogot <samogot@gmail.com>
2015-05-17 17:45:18 +0300

    prototype of comments panel

---
+++ src/main/java/ru/ruranobe/wicket/components/CommentsPanel.java
@@ -0,0 +1,14 @@
+package ru.ruranobe.wicket.components;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Created by Samogot on 16.05.2015.
+ */
+public class CommentsPanel extends Panel
+{
+    public CommentsPanel(String id)
+    {
+        super(id);
+    }
+}
