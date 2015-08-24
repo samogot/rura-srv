875c35231bb5d1fd7b0a866f27c6f5e678dc71df
gostevv <timelimit@mail.ru>
2015-08-24 23:53:53 +0400

    draft for bookmarks and orphus implemented. setting of id and other attributes of <p> tag fixed.
    *forgotten files addded.

---
+++ src/main/java/ru/ruranobe/mybatis/mappers/BookmarksMapper.java
@@ -0,0 +1,8 @@
+package ru.ruranobe.mybatis.mappers;
+
+import ru.ruranobe.mybatis.tables.Bookmark;
+
+public interface BookmarksMapper
+{
+    public void insertBookmark(Bookmark bookmark);
+}
---
+++ src/main/java/ru/ruranobe/wicket/resources/BookmarksRestWebService.java
@@ -0,0 +1,61 @@
+package ru.ruranobe.wicket.resources;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.wicketstuff.rest.annotations.MethodMapping;
+import org.wicketstuff.rest.annotations.parameters.RequestBody;
+import org.wicketstuff.rest.resource.gson.GsonRestResource;
+import org.wicketstuff.rest.utils.http.HttpMethod;
+import ru.ruranobe.mybatis.MybatisUtil;
+import ru.ruranobe.mybatis.mappers.BookmarksMapper;
+import ru.ruranobe.mybatis.mappers.ChaptersMapper;
+import ru.ruranobe.mybatis.mappers.cacheable.CachingFacade;
+import ru.ruranobe.mybatis.tables.Bookmark;
+import ru.ruranobe.mybatis.tables.Chapter;
+
+import java.util.Date;
+
+public class BookmarksRestWebService extends GsonRestResource
+{
+    @MethodMapping(value = "/insert", httpMethod = HttpMethod.POST)
+    public void insertBookmark(@RequestBody Bookmark bookmark)
+    {
+        /*
+        TODO: switched off since there is not authorization yet.
+        if (bookmark.getUserId() == null)
+        {
+            throw new IllegalArgumentException("userId wasn't specified.");
+        }*/
+        if (bookmark.getParagraphId() == null)
+        {
+            throw new IllegalArgumentException("paragraphId wasn't specified.");
+        }
+
+        if (bookmark.getChapterId() == null)
+        {
+            throw new IllegalArgumentException("chapterId wasn't specified.");
+        }
+
+        SqlSessionFactory sessionFactory = MybatisUtil.getSessionFactory();
+        SqlSession session = sessionFactory.openSession();
+        try
+        {
+            ChaptersMapper chaptersMapperCacheable = CachingFacade.getCacheableMapper(session, ChaptersMapper.class);
+            Chapter chapter = chaptersMapperCacheable.getChapterById(bookmark.getChapterId());
+            if (chapter == null)
+            {
+                throw new IllegalArgumentException("Chapter with the specified chapterId doesn't exist.");
+            }
+
+            BookmarksMapper bookmarksMapperCacheable = CachingFacade.getCacheableMapper(session, BookmarksMapper.class);
+            bookmark.setInsertionTime(new Date());
+            bookmarksMapperCacheable.insertBookmark(bookmark);
+            session.commit();
+        }
+        finally
+        {
+            session.close();
+        }
+    }
+
+}
