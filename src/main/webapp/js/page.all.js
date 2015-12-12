6ae9f3e8e6f4e281b637af3e281f0dff1aeffd7c
Antosik <grig_anton96@mail.ru>
2015-12-12 14:14:15 +0300

    Added night theme
    Added night theme to all pages

--- src/main/webapp/js/page.all.js
+++ src/main/webapp/js/page.all.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $('.module_name').click(function () {
+$(document).ready(function() {
+    $('.module_name').click(function() {
         $(this).children('.fa').toggleClass("fa-chevron-right fa-chevron-down").end()
             .toggleClass("opened").next('.actions').slideToggle($('#contents-module').length ? reinitAffix : undefined);
     });
@@ -7,17 +7,128 @@ $(document).ready(function () {
     $('.ellipses-left').trimLeft();
 
     var banH = $('#all-projects-module .banners a').height();
-    var lasts = $('.module').last().offset().top + $('.module').last().height();
-    if (lasts > $('.leftColumn .content').height()) {
-        var needDelete = parseInt((((lasts - $('.leftColumn .content').height()) / $('#all-projects-module .banners a').height()) + 1), 10);
-        var Blength = $('#all-projects-module .banners a').length;
-        var deleteFrom = Blength - needDelete + 1;
-        if (deleteFrom < 4) deleteFrom = 4;
-        $('#all-projects-module .banners a').slice(deleteFrom + 1).hide();
-        $('#all-projects-module .banners').append('<span class="more moreprojects">Больше</span>');
-        $('.moreprojects').click(function () {
-            $(this).remove();
-            $('#all-projects-module .banners a:hidden').slideDown($('#contents-module').length ? reinitAffix : undefined);
+    if ($('.module').length != 0) {
+        var lasts = $('.module').last().offset().top + $('.module').last().height();
+        if (lasts > $('.leftColumn .content').height()) {
+            var needDelete = parseInt((((lasts - $('.leftColumn .content').height()) / $('#all-projects-module .banners a').height()) + 1), 10);
+            var Blength = $('#all-projects-module .banners a').length;
+            var deleteFrom = Blength - needDelete + 1;
+            if (deleteFrom < 4) deleteFrom = 4;
+            $('#all-projects-module .banners a').slice(deleteFrom + 1).hide();
+            $('#all-projects-module .banners').append('<span class="more moreprojects">Больше</span>');
+            $('.moreprojects').click(function() {
+                $(this).remove();
+                $('#all-projects-module .banners a:hidden').slideDown($('#contents-module').length ? reinitAffix : undefined);
+            });
+        }
+    }
+    if ($('.miniSearch').length != 0 || $('#main-search').length != 0) {
+        var element = $('.miniSearch').length != 0 ? $('.miniSearch') : $('#main-search')
+        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js', function() {
+            element.children('input').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            }, {
+                name: 'projects',
+                display: 'title',
+                source: function(query, syncResults, asyncResults) {
+                    $.get('/api/projects/get/all?params=name_ru;url;name_jp;name_romaji;title', function(data) {
+                        var matches, substringRegex;
+                        matches = [];
+                        substrRegex = new RegExp(query, 'i');
+                        $.each(data, function(i, str) {
+                            if (str == null) return;
+                            if (substrRegex.test(str.title) || substrRegex.test(str.nameEn) || substrRegex.test(str.nameRu) || substrRegex.test(str.nameJp) || substrRegex.test(str.Romaji)) {
+                                str.nameEn = (str.nameEn == undefined) ? '' : str.nameEn;
+                                str.nameRu = (str.nameRu == undefined) ? '' : str.nameRu;
+                                str.nameJp = (str.nameJp == undefined) ? '' : str.nameJp;
+                                str.nameRomaji = (str.nameRomaji == undefined) ? '' : str.nameRomaji;
+                                matches.push({
+                                    title: str.title,
+                                    nameEn: str.nameEn,
+                                    nameRu: str.nameRu,
+                                    nameJp: str.nameJp,
+                                    nameRomaji: str.nameRomaji,
+                                    link: str.url
+                                });
+                            }
+                        });
+                        asyncResults(matches);
+                    });
+                },
+                templates: {
+                    empty: function(data) {
+                        return [
+                            '<div class="empty-message" align="center" style="width:100%">',
+                            'Извините, данный проект не найден :С',
+                            '</div>'
+                        ].join('\n')
+                    },
+                    suggestion: function(data) {
+                        return '<p style="cursor:pointer"><strong>' + data.title + '</strong><small class="text-muted" style="font-size:12px"><br>' + data.nameEn + ' ' + data.nameJp + ' ' + data.nameRu + ' ' + data.nameRomaji + '</small></p>';
+                    }
+                }
+            }).bind('typeahead:select', function(ev, selection) {
+                location.href = '/r/' + selection.link;
+            }).bind('typeahead:autocomplete', function(ev, selection) {
+                location.href = '/r/' + selection.link;
+            });
+
+            element.children('button').click(function() {
+                location.href = "https://cse.google.ru/cse/publicurl?cx=016828743293566058131:ctxseqkthgk&q=" + element.children('.tt-input').val();;
+            });
         });
     }
+    
 });
+
+ function supportsLocalStorage() {
+     try {
+         return 'localStorage' in window && window['localStorage'] !== null;
+     } catch (e) {
+         return false;
+     }
+ }
+
+ function saveSettings(options) {
+     if (!supportsLocalStorage()) {
+         return false;
+     }
+     localStorage.setItem(options.key, options.item);
+ }
+
+ function loadSettings() {
+     if (!supportsLocalStorage()) {
+         return false;
+     }
+     if (localStorage.getItem("night") == "true") {
+         $('body').addClass("night");
+         $('a.navbar-brand img').attr('src', 'img/logo1_night.png');
+         $('.daynight-button .fa').toggleClass('fa-sun-o fa-moon-o');
+     } else {
+         $('body').removeClass("night");
+         $('a.navbar-brand img').attr('src', 'img/logo1.png');
+     }
+ }
+ $(document).ready(function() {
+     loadSettings()
+ });
+ $('.daynight-button').on('click', function(e) {
+     if ($(this).children('.fa').hasClass('fa-sun-o')) {
+         $('body').addClass("night");
+         $('a.navbar-brand img').attr('src', 'img/logo1_night.png');
+         saveSettings({
+             key: 'night',
+             item: true
+         });
+     } else {
+         $('body').removeClass("night");
+         $('a.navbar-brand img').attr('src', 'img/logo1.png');
+         saveSettings({
+             key: 'night',
+             item: false
+         });
+     }
+     $(this).children('.fa').toggleClass('fa-sun-o fa-moon-o');
+ });
