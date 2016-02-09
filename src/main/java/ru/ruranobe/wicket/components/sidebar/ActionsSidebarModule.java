1775212b90daff23aaceb5424f104de976b5c30e
Pavel Pavlov <pavel.pavlov@cheb.wikimart.ru>
2016-02-10 02:01:12 +0300

    NPE fix

--- src/main/java/ru/ruranobe/wicket/components/sidebar/ActionsSidebarModule.java
+++ src/main/java/ru/ruranobe/wicket/components/sidebar/ActionsSidebarModule.java
@@ -2,24 +2,23 @@ package ru.ruranobe.wicket.components.sidebar;
 
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import ru.ruranobe.wicket.LoginSession;
 import ru.ruranobe.wicket.webpages.admin.Orphus;
 
-public class ActionsSidebarModule extends SidebarModuleBase
-{
-    private String project = null;
+public class ActionsSidebarModule extends SidebarModuleBase {
 
-    public ActionsSidebarModule(Class editClass, PageParameters pageParameters)
-    {
-        super("sidebarModule", "actions", "Действия");
-        project = pageParameters.get("project").toOptionalString();
-        moduleBody.add(new BookmarkablePageLink("edit", editClass, pageParameters));
-        moduleBody.add(new BookmarkablePageLink("orphus", Orphus.class, pageParameters));
-    }
+	private String project = null;
 
-    @Override
-    public boolean isVisible()
-    {
-        return LoginSession.get().isProjectEditAllowedByUser(project);
-    }
+	public ActionsSidebarModule(Class editClass, PageParameters pageParameters) {
+		super("sidebarModule", "actions", "Действия");
+		project = pageParameters != null ? pageParameters.get("project").toOptionalString() : null;
+		moduleBody.add(new BookmarkablePageLink("edit", editClass, pageParameters));
+		moduleBody.add(new BookmarkablePageLink("orphus", Orphus.class, pageParameters));
+	}
+
+	@Override
+	public boolean isVisible() {
+		return LoginSession.get().isProjectEditAllowedByUser(project);
+	}
 }
