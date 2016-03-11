684384b1cbf1b11e73c7642dce2db017ddf59c93
Samogot <samogot@gmail.com>
2016-03-11 09:59:42 +0200

    radio api enchant

--- src/main/java/ru/ruranobe/config/ApplicationContext.java
+++ src/main/java/ru/ruranobe/config/ApplicationContext.java
@@ -23,7 +23,7 @@ public class ApplicationContext
         loadFileStorageServiceConfig(document);
         loadWebPageConfig(document);
         loadSmtpConfig(document);
-        loadForumApiSecret(document);
+        loadTokensConfig(document);
     }
 
     public static String getForumApiSecret()
@@ -31,10 +31,17 @@ public class ApplicationContext
         return forumApiSecret;
     }
 
-    private void loadForumApiSecret(Document document)
+    public static String getRadioApiToken()
+    {
+        return radioApiToken;
+    }
+
+    private void loadTokensConfig(Document document)
     {
         XPath xpath = new DefaultXPath("/Configuration/ForumApiSecret");
         forumApiSecret = xpath.selectSingleNode(document).getText();
+        xpath = new DefaultXPath("/Configuration/RadioApiToken");
+        radioApiToken = xpath.selectSingleNode(document).getText();
     }
 
     private void loadWebPageConfig(Document document)
@@ -149,6 +156,7 @@ public class ApplicationContext
     private static final String FILE_STORAGE_UPLOAD_DIR = "UploadDir";
     private static final String FILE_STORAGE_PUBLIC_FOLDER = "PublicFolder";
     private static String forumApiSecret;
+    private static String radioApiToken;
     /*private static final String FILE_STORAGE_LOGIN = "Login";
     private static final String FILE_STORAGE_PASSWORD = "Password";
     private static final String FILE_STORAGE_FILE_EXTENSION = "FileExtension";*/
--- src/main/java/ru/ruranobe/wicket/resources/rest/RadioRestWebService.java
+++ src/main/java/ru/ruranobe/wicket/resources/rest/RadioRestWebService.java
@@ -1,36 +1,66 @@
 package ru.ruranobe.wicket.resources.rest;
 
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.apache.wicket.request.http.WebResponse;
+import org.apache.wicket.util.string.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wicketstuff.rest.annotations.MethodMapping;
 import org.wicketstuff.rest.annotations.ResourcePath;
 import org.wicketstuff.rest.annotations.parameters.RequestParam;
 import org.wicketstuff.rest.resource.gson.GsonRestResource;
+import ru.ruranobe.config.ApplicationContext;
+
+import java.util.Collection;
 
 @ResourcePath("/api/radio")
 public class RadioRestWebService extends GsonRestResource
 {
 
-    @MethodMapping("/nowplaying/{token}")
-    public NowPlayingInfo setNowPlayingInfo(String token,
-                                            @RequestParam(value = "title", required = false) String title,
-                                            @RequestParam(value = "artist", required = false) String artist,
-                                            @RequestParam(value = "duration", required = false) String duration)
-    {
-        if (token.equals("123") && title != null)
-        {
-            nowPlayingInfo = new NowPlayingInfo();
-            nowPlayingInfo.setArtist(artist);
-            nowPlayingInfo.setTitle(title);
-            nowPlayingInfo.setDuration(duration);
-        }
-        return nowPlayingInfo;
-    }
-
     @MethodMapping("/nowplaying")
-    public NowPlayingInfo getNowPlayingInfo()
+    public Collection<NowPlayingInfo> setNowPlayingInfo(@RequestParam(value = "token", required = false) String token,
+                                                        @RequestParam(value = "title", required = false) String title,
+                                                        @RequestParam(value = "artist", required = false) String artist,
+                                                        @RequestParam(value = "len", required = false) String len,
+                                                        @RequestParam(value = "playcount", required = false) Integer playcount,
+                                                        @RequestParam(value = "listeners", required = false) Integer listeners,
+                                                        @RequestParam(value = "showname", required = false) String showname)
     {
+        if (ApplicationContext.getRadioApiToken().equals(token))
+        {
+            synchronized (nowPlayingInfo)
+            {
+                NowPlayingInfo info;
+                boolean toadd = false;
+                if (!nowPlayingInfo.isEmpty())
+                {
+                    info = nowPlayingInfo.get(nowPlayingInfo.size() - 1);
+                    if (!Strings.isEqual(info.title, title) ||
+                        !Strings.isEqual(info.artist, artist) ||
+                        !Strings.isEqual(info.len, len))
+                    {
+                        info = new NowPlayingInfo();
+                        toadd = true;
+                    }
+                }
+                else
+                {
+                    info = new NowPlayingInfo();
+                    toadd = true;
+                }
+                info.title = title;
+                info.artist = artist;
+                info.len = len;
+                info.playcount = playcount;
+                info.listeners = listeners;
+                info.showname = showname;
+                if (toadd)
+                {
+                    nowPlayingInfo.add(info);
+                }
+            }
+        }
+
         return nowPlayingInfo;
     }
 
@@ -41,43 +71,16 @@ public class RadioRestWebService extends GsonRestResource
         LOG.error("Error in REST API call", exception);
     }
 
-    class NowPlayingInfo
+    private class NowPlayingInfo
     {
-        public String getTitle()
-        {
-            return title;
-        }
-
-        public void setTitle(String title)
-        {
-            this.title = title;
-        }
-
-        public String getArtist()
-        {
-            return artist;
-        }
-
-        public void setArtist(String artist)
-        {
-            this.artist = artist;
-        }
-
-        public String getDuration()
-        {
-            return duration;
-        }
-
-        public void setDuration(String duration)
-        {
-            this.duration = duration;
-        }
-
-        String title;
-        String artist;
-        String duration;
+        public String title;
+        public String artist;
+        public String len;
+        public Integer playcount;
+        public Integer listeners;
+        public String showname;
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(RadioRestWebService.class);
-    static NowPlayingInfo nowPlayingInfo;
+    private static final CircularFifoQueue<NowPlayingInfo> nowPlayingInfo = new CircularFifoQueue<>(10);
 }
