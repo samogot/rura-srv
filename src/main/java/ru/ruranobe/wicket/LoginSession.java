488fbc8ee42e48bb5814cfb8cef3e86ed9d3cecd
Samogot <samogot@gmail.com>
2016-03-13 11:22:01 +0200

    commit password version rise

--- src/main/java/ru/ruranobe/wicket/LoginSession.java
+++ src/main/java/ru/ruranobe/wicket/LoginSession.java
@@ -83,6 +83,7 @@ public class LoginSession extends AuthenticatedWebSession
                     authenticationCompleted = true;
                 }
             }
+            session.commit();
         }
         return authenticationCompleted;
     }
